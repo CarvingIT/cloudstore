ac2f0d7b7fc99feba6969818adeae6c4cf69bdd3
Ketan Kulkarni <kk@ampra.in>
2020-08-06 08:35:02 +0530

    test edit

--- resources/views/sources.blade.php
+++ resources/views/sources.blade.php
@@ -29,6 +29,12 @@
                                 </select>
                             </div>
                         </div>
+                        <div class="form-group row sshfield">
+                            <label for="server" class="col-md-4 col-form-label text-md-right">{{ __('Server') }}</label>
+                            <div class="col-md-6">
+                                <input id="server" type="text" class="form-control" name="server" value="" placeholder="FQDN or IP Address">
+                            </div>
+                        </div>
                         <div class="form-group row">
                             <label for="path" class="col-md-4 col-form-label text-md-right">{{ __('Path') }}</label>
                             <div class="col-md-6">
@@ -95,6 +101,9 @@
                             { "orderable": false, "targets": 3 }
                             ]
                         });
+                        // hide all ssh fields
+                        $('.sshfield').hide();
+    
                     } );
                     
                     $( function() {
