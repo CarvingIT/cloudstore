7b11c04aee2dde45dae7631086a2807b11285e72
Ketan Kulkarni <kk@ampra.in>
2020-08-06 19:02:50 +0530

    Added forms for configuring backup over ssh, ftp

--- resources/views/sources.blade.php
+++ resources/views/sources.blade.php
@@ -5,28 +5,13 @@
         <div class="col-md-8">
             <div id="sourceform" title="Source info" class="card">
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
                     <form method="POST" action="/admin/source/save">
                         @csrf
                         <div class="form-group row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="type" class="col-md-4 col-form-label text-md-right">{{ __('Type') }}</label>
-                            <div class="col-md-6">
-                                <select class="form-control" name="type">
-                                    <option value="local">Local</option>
-                                    <option value="ssh">Remote (Secure Shell)</option>
-                                    <option value="ftp">Remote (FTP)</option>
-                                </select>
+                                <input type="hidden" name="type" value="local" />
                             </div>
                         </div>
                         <div class="form-group row">
@@ -56,11 +41,144 @@
                     </form>
                 </div>
             </div>
+
+            <div id="sourceformssh" title="Configure back up of remote directory over SSH" class="card">
+                <div class="card-body">
+                    <form method="POST" action="/admin/source/save">
+                        @csrf
+                        <div class="form-group row">
+                            <label for="namessh" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                            <div class="col-md-6">
+                                <input id="namessh" type="text" class="form-control" name="namessh" value="" required>
+                                <input type="hidden" name="type" value="ssh" />
+                            </div>
+                        </div>
+                        <hr />
+                        <div class="form-group row">
+                            <label for="serverssh" class="col-md-4 col-form-label text-md-right">{{ __('Server address') }}</label>
+                            <div class="col-md-6">
+                                <input id="serverssh" type="text" class="form-control" name="serverssh" value="" placeholder="FQDN or IP Address" required>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="portssh" class="col-md-4 col-form-label text-md-right">{{ __('Port') }}</label>
+                            <div class="col-md-6">
+                                <input id="portssh" type="text" class="form-control" name="portssh" value="">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="usernamessh" class="col-md-4 col-form-label text-md-right">{{ __('SSH Username') }}</label>
+                            <div class="col-md-6">
+                                <input id="usernamessh" type="text" class="form-control" name="usernamessh" value="" required>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="passwordssh" class="col-md-4 col-form-label text-md-right">{{ __('SSH Password') }}</label>
+                            <div class="col-md-6">
+                                <input id="passwordssh" type="password" class="form-control" name="passwordssh" value="" required>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="pathssh" class="col-md-4 col-form-label text-md-right">{{ __('Remote directory') }}</label>
+                            <div class="col-md-6">
+                                <input id="pathssh" type="text" class="form-control" name="pathssh" value="{{ old('path') }}" required>
+                            </div>
+                        </div>
+                        <hr />
+                        <div class="form-group row">
+                            <label for="drive_idssh" class="col-md-4 col-form-label text-md-right">{{ __('Mapped Drive') }}</label>
+                            <div class="col-md-6">
+                                <select class="form-control" id="drive_idssh" name="drive_idssh">
+                                @foreach($drives as $drive)
+                                <option value="{{ $drive->id }}">{{ $drive->name }}</option>
+                                @endforeach
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Save') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <div id="sourceformftp" title="Configure remote directory for back up over FTP" class="card">
+                <div class="card-body">
+                    <form method="POST" action="/admin/source/save">
+                        @csrf
+                        <div class="form-group row">
+                            <label for="nameftp" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                            <div class="col-md-6">
+                                <input id="nameftp" type="text" class="form-control" name="nameftp" value="" required>
+                                <input type="hidden" name="type" value="ftp" />
+                            </div>
+                        </div>
+                        <hr />
+                        <div class="form-group row">
+                            <label for="serverftp" class="col-md-4 col-form-label text-md-right">{{ __('Server address') }}</label>
+                            <div class="col-md-6">
+                                <input id="serverftp" type="text" class="form-control" name="serverftp" value="" placeholder="FQDN or IP Address" required>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="portftp" class="col-md-4 col-form-label text-md-right">{{ __('Port') }}</label>
+                            <div class="col-md-6">
+                                <input id="portftp" type="text" class="form-control" name="portftp" value="">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="usernameftp" class="col-md-4 col-form-label text-md-right">{{ __('FTP Username') }}</label>
+                            <div class="col-md-6">
+                                <input id="usernameftp" type="text" class="form-control" name="usernameftp" value="" required>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="passwordftp" class="col-md-4 col-form-label text-md-right">{{ __('FTP Password') }}</label>
+                            <div class="col-md-6">
+                                <input id="passwordftp" type="password" class="form-control" name="passwordftp" value="" required>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="pathftp" class="col-md-4 col-form-label text-md-right">{{ __('Remote directory') }}</label>
+                            <div class="col-md-6">
+                                <input id="pathftp" type="text" class="form-control" name="pathftp" value="" required>
+                            </div>
+                        </div>
+                        <hr />
+                        <div class="form-group row">
+                            <label for="drive_idftp" class="col-md-4 col-form-label text-md-right">{{ __('Mapped Drive') }}</label>
+                            <div class="col-md-6">
+                                <select id="drive_idftp" class="form-control" name="drive_idftp">
+                                @foreach($drives as $drive)
+                                <option value="{{ $drive->id }}">{{ $drive->name }}</option>
+                                @endforeach
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Save') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
             <div class="card">
                 <div class="card-header">
                 <a href="/admin/dashboard">Administration</a> :: {{ __('Sources') }}
                 <div class="card-header-icons">
-                    <a href="#" id="loadform"><span class="ui-icon ui-icon-plusthick"></span></a>
+                    <a href="#" id="loadform"><img src="/img/new_folder.png" /></a>
+                    <a href="#" id="loadformftp"><img src="/img/ftp.png" /></a>
+                    <a href="#" id="loadformssh"><img src="/img/ssh.png" /></a>
                 </div>
                 </div>
                 <div class="card-body">
@@ -106,8 +224,31 @@
                                 modal: true,
                             }
                         );
+                        dialogssh = $( "#sourceformssh" ).dialog(
+                            {
+                                autoOpen: false,
+                                height: 550,
+                                width: 500,
+                                modal: true,
+                            }
+                        );
+                        dialogftp = $( "#sourceformftp" ).dialog(
+                            {
+                                autoOpen: false,
+                                height: 550,
+                                width: 500,
+                                modal: true,
+                            }
+                        );
+
                         $( "#loadform" ).on( "click", function() {
                             dialog.dialog( "open" );
+                        });
+                        $( "#loadformssh" ).on( "click", function() {
+                            dialogssh.dialog( "open" );
+                        });
+                        $( "#loadformftp" ).on( "click", function() {
+                            dialogftp.dialog( "open" );
                         });
                     } );
 
